bcda3ada4d7df319f2947037129a67d310a16802
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-02-06 02:57:45 +0800

    refactor employee controller to simplify response structure in getEmployees function

--- src/controllers/employee_controller.ts
+++ src/controllers/employee_controller.ts
@@ -1,10 +1,11 @@
 import Employee from "../models/employee.ts";
 
+//for employee page
 export const getEmployees = async (req: any, res: any) => {
   try {
     const employee = await Employee.findAll();
 
-    res.status(200).json({ message: "Employee fetch successfully.", employee });
+    res.status(200).json(employee);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
