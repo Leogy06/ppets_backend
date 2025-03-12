1450f6ec944fca0702861d2b394dbc5734b9f9d6
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-03-12 13:32:02 +0800

    fall backdate

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,6 +1,10 @@
 import { format } from "date-fns";
 
 export const dateFormatter = (date: string, formatString = "MM/dd/yyyy") => {
+  if (!date) {
+    return "";
+  }
+
   const formattedDate = format(new Date(date), formatString);
 
   return formattedDate;
