a4eaba79d44146353c5ca6d484dcf5a7b11ac1db
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-05-05 11:31:50 +0800

    new update for items

--- src/middlewares/itemValidations.ts
+++ src/middlewares/itemValidations.ts
@@ -11,8 +11,8 @@ const itemValidationRules = [
   body("PROP_NO").notEmpty().withMessage("Property Number is missing."),
   body("DEPARTMENT_ID").notEmpty().withMessage("Department ID is missing."),
   body("RECEIVED_AT").notEmpty().withMessage("Received date is missing."),
-  body("PAR_NO").notEmpty().withMessage("PAR number is missing."),
-  body("MR_NO").notEmpty().withMessage("Mr number is missing."),
+  body("PAR_NO").optional(),
+  body("MR_NO").optional(),
   body("ACCOUNT_CODE").notEmpty().withMessage("Account code is missing."),
 ];
 
